9859a97c1b9c9c1edd3119a74fc3b0e29a660b7b
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2025-12-25 20:28:29 +0530

    feat: Add file reading functionality to ReadFiles.java and include example text files.

--- src/lambdasSyntax/ReadFiles.java
+++ src/lambdasSyntax/ReadFiles.java
@@ -1,4 +1,37 @@
 package lambdasSyntax;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class ReadFiles {
+    static void main() throws IOException {
+        List<String> fileNames = List.of("file-a.txt", "file-b.txt", "file-c.txt");
+
+        // Imperative way of writing the code
+        for (String fileName : fileNames) {
+            for (String line : Files.readAllLines(Paths.get(fileName),
+                    StandardCharsets.UTF_8)) {
+                System.out.println(line);
+            }
+        }
+
+        // Functional way of writing the code
+        Stream.of("file-a.txt", "file-b.txt", "file-c.txt")
+                .map(Path::of)
+                .map(p -> {
+                    try {
+                        return Files.readAllLines(p, StandardCharsets.UTF_8);
+                    } catch (IOException e) {
+                        return List.of();
+                    }
+                })
+                .flatMap(Collection::stream)
+                .forEach(System.out::println);
+    }
 }
